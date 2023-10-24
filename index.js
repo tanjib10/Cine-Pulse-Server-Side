b5c28f79c445993e8ddbfd461cd061e449ef29f5
tanjib10 <radiulmd@gmail.com>
2023-10-24 13:56:19 +0600

    loaded data from mongoDB

--- index.js
+++ index.js
@@ -27,13 +27,22 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
+
+    const brandCollection = client.db('cine-pulse').collection('brand-products');
+
+    app.get('/brand', async (req, res) => {
+      const cursor = brandCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
